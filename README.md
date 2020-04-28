eafc20147341382b4f8ca21b65dc7b925386a1c3
Artemso <solopovtema@gmail.com>
2020-04-28 15:16:33 +0300

    updated visuals, edited readme

--- README.md
+++ README.md
@@ -7,6 +7,13 @@ A complementary data analysis project for my python Instagram bot
 
 ## Built using:
 * Bokeh – used for data visualisation
-* Networkx – used to create a graph from obtained list of followers
+* NetworkX – used to create a graph from obtained list of followers
 
 This project gave me an interesting insight on connections between my friends and their relationships with each other.
+
+### Things noted:
+* Friend group clustering which is clearly visible
+* Famous figures and brands tend to locate in the central area connecting different groups of people
+
+### Use cases:
+* One could target a marketing campaign towards users who share similar interests and/or connected with each other.
--- analyse_json.py
+++ analyse_json.py
@@ -1,12 +1,13 @@
 import json
-import plotly.graph_objects as go
 import networkx as nx
 from bokeh.io import output_file, show
 from bokeh.models import Plot, Range1d, MultiLine, Circle, HoverTool, TapTool, BoxSelectTool, PanTool, WheelZoomTool, ResetTool
-from bokeh.models import HoverTool, TapTool, BoxSelectTool
-from bokeh.models.graphs import NodesAndLinkedEdges, EdgesAndLinkedNodes
-from bokeh.palettes import Spectral4
+from bokeh.models.graphs import NodesAndLinkedEdges
+from bokeh.palettes import Plasma11
+from bokeh.palettes import Greys4
 from bokeh.plotting import from_networkx
+from bokeh.transform import linear_cmap
+from bokeh.models import ColumnDataSource
 
 class Visualise():
 	def __init__(self):
@@ -45,16 +46,18 @@ class Visualise():
 			for underkey in user_dict[key]:
 				graph.add_edge(key, underkey)
 
+	def get_list_of_edges(self, graph):
+		edge_list = []
+		for node in graph.nodes():
+			edge_list.append(len(list(graph.neighbors(node))))
+		return edge_list
+
 	def network_total_following(self, user_dict):
 		graph = nx.Graph()
 		self.add_nodes(graph, user_dict)
 		self.add_edges(graph, user_dict)
-		# graph.remove_node('artmslpv')
 		for node in list(graph.nodes()):
-			if len(list(graph.neighbors(node))) < 2:
-				graph.remove_node(node)
-		for node in list(graph.nodes()):
-			if len(list(graph.neighbors(node))) < 7:
+			if len(list(graph.neighbors(node))) < 5:
 				graph.remove_node(node)
 		plot = Plot(
 			plot_width=1140,
@@ -62,7 +65,8 @@ class Visualise():
 			x_range=Range1d(-1.1,1.1),
 			y_range=Range1d(-1.1,1.1))
 		plot.add_tools(
-			HoverTool(tooltips=[('','@name')],
+			HoverTool(tooltips=[('User','@name'),
+				('Neighbors', '@neighbors')],
 				line_policy='nearest'),
 			TapTool(),
 			BoxSelectTool(),
@@ -71,73 +75,34 @@ class Visualise():
 			ResetTool())
 		graph_render = from_networkx(graph, nx.spring_layout, scale=1.8, center=(0,0))
 		graph_render.node_renderer.data_source.data['name'] = list(graph.nodes())
+		list_edges = self.get_list_of_edges(graph)
+		graph_render.node_renderer.data_source.data['neighbors'] = list_edges
+		print(min(list_edges))
+		print(max(list_edges))
+		mapper = linear_cmap('neighbors', palette=Plasma11, low=min(list_edges), high=max(list_edges))
 		graph_render.node_renderer.glyph = Circle(
 			size=10,
-			fill_color=Spectral4[0])
+			fill_color=mapper)
 		graph_render.node_renderer.hover_glyph = Circle(
-			size=15,
-			fill_color=Spectral4[1])
+			size=200,
+			fill_color=mapper)
 		graph_render.node_renderer.selection_glyph = Circle(
-			size=20,
-			fill_color=Spectral4[2])
+			size=200,
+			fill_color=mapper)
 		graph_render.edge_renderer.glyph = MultiLine(
-			line_color="#CCCCCC",
-			line_alpha=0.2,
-			line_width=2)
+			line_color=Greys4[2],
+			line_alpha=0.3,
+			line_width=1)
 		graph_render.edge_renderer.hover_glyph = MultiLine(
-			line_color=Spectral4[1],
-			line_width=3)
+			line_color=Greys4[1],
+			line_width=2)
 		graph_render.edge_renderer.selection_glyph = MultiLine(
-			line_color=Spectral4[2],
-			line_width=5)
+			line_color=Greys4[0],
+			line_width=3)
 		graph_render.selection_policy = NodesAndLinkedEdges()
 		graph_render.inspection_policy = NodesAndLinkedEdges()
 		plot.renderers.append(graph_render)
 		show(plot)
-		# pos = nx.spring_layout(graph, k=0.5, iterations=20)
-		# edge_x = []
-		# edge_y = []
-		# for edge in graph.edges():
-		# 	x0, y0 = pos.get(edge[0])
-		# 	x1, y1 = pos.get(edge[1])
-		# 	edge_x.append(x0)
-		# 	edge_x.append(x1)
-		# 	edge_x.append(None)
-		# 	edge_y.append(y0)
-		# 	edge_y.append(y1)
-		# 	edge_y.append(None)
-		# edge_trace = go.Scatter(
-		# 	x =edge_x, y = edge_y,
-		# 	line=dict(width=0.5, color='#888'),
-		# 	hoverinfo='none',
-		# 	mode='lines'
-		# )
-		# node_x = []
-		# node_y = []
-		# for node in graph.nodes():
-		# 	x, y = pos.get(node)
-		# 	node_x.append(x)
-		# 	node_y.append(y)
-		# node_trace = go.Scatter(
-		# 	x = node_x, y = node_y,
-		# 	mode='markers',
-		# 	hoverinfo='text'
-		# )
-		# node_trace.text = list(graph.nodes())
-		# fig = go.Figure(data=[edge_trace, node_trace],
-		# layout=go.Layout(
-		# 	title='Network graph of people I\'m following',
-		# 	titlefont_size=16,
-		# 	showlegend=False,
-		# 	hovermode='closest',
-		# 	margin=dict(b=20,l=5,r=5,t=40),
-		# 	xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
-        #     yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
-		# ))
-		# fig.show()
-
-
-
 
 with open('user_dict.json', 'r') as fd:
 	user_dict = json.load(fd)
